9e737c5b6969878f303570f000f3c6afd2050add
Calvin Lapp <lapp.cd@gmail.com>
2024-02-20 20:25:40 -0500

    Added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
-export default defineConfig({ integrations: [tailwind()] });
+export default defineConfig({ integrations: [tailwind(), svelte()] });
