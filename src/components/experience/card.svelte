d141baff89cd5c3dd12aa4e1ae6153a4d59f8a13
Calvin Lapp <lapp.cd@gmail.com>
2024-02-22 21:17:46 -0500

    Initial experience skeleton

---
+++ src/components/experience/card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let from: string;
+    export let till = 'present';
+    export let title: string;
+    export let companyName: string;
+    export let link: string;
+</script>
+
+<div class="group hover:bg-[#1e2129] p-12 grid sm:grid-cols-8">
+    <!-- Date -->
+    <div class="w-full sm:col-span-2 px-4 py-2">
+        <p
+            class="w-full text-right uppercase font-semibold tracking-wide text-slate-500 text-sm"
+        >
+            {from} — {till}
+        </p>
+    </div>
+    <div class="sm:col-span-6 px-4 py-2">
+        <h2 class="font-medium leading-snug">
+            <a
+                href={link}
+                class="leading-tight text-slate-200 font-semibold group/link text-lg"
+                target="_blank"
+                aria-label={`${title} at ${companyName} (opens in a new tab)`}
+                >{title} | {companyName}</a
+            >
+        </h2>
+        <p class="mt-2 text-md leading-normal"><slot /></p>
+    </div>
+</div>
--- src/components/experience/chillin.svelte
+++ src/components/experience/chillin.svelte
@@ -1,9 +1,44 @@
 <script>
+    import JobCard from './card.svelte';
     import ContentWrapper from '../content-wrapper.svelte';
     import Title from '../title.svelte';
 </script>
 
 <ContentWrapper class="flex flex-col gap-4">
     <Title>Experience</Title>
-    <div class="hover:bg-[#1e2129] p-12 flex"></div>
+    <JobCard
+        title="Senior Software Engineer"
+        companyName="PostGrid"
+        link="https://postgrid.com"
+        from="Sept 2023"
+        >Hey there. Just gonna put some random jargon here for a lil bit to take
+        up some space I guess. I did some stuff</JobCard
+    >
+    <JobCard
+        title="Backend Engineer"
+        companyName="PostGrid"
+        link="https://postgrid.com"
+        from="Feb 2022"
+        till="Sept 2023"
+        >Hey there. Just gonna put some random jargon here for a lil bit to take
+        up some space I guess. I did some stuff</JobCard
+    >
+    <JobCard
+        title="React Native Developer"
+        companyName="A.I. Hub, Durham College"
+        link="https://durhamcollege.ca/about/office-of-research-services-innovation-and-entrepreneurship-orsie"
+        from="Feb 2022"
+        till="Sept 2023"
+        >Hey there. Just gonna put some random jargon here for a lil bit to take
+        up some space I guess. I did some stuff</JobCard
+    >
+    <JobCard
+        title="Front-End Engineer"
+        companyName="A.I. Hub, Durham College"
+        link="https://durhamcollege.ca/about/office-of-research-services-innovation-and-entrepreneurship-orsie"
+        from="Feb 2022"
+        till="Sept 2023"
+        >Hey there. Just gonna put some random jargon here for a lil bit to take
+        up some space I guess. I did some stuff</JobCard
+    >
 </ContentWrapper>
