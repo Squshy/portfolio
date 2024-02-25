5f1be2967f7317954832412e65708e42117e824e
Calvin Lapp <lapp.cd@gmail.com>
2024-02-25 17:50:00 -0500

    Added github links and descs for projects

--- src/components/projects/card.svelte
+++ src/components/projects/card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Card from '../card.svelte';
+    import Icon from '../icon.svelte';
 
     // TODO: Figure out why these types aren't yelling at me if one is not
     // provided? I have strict mode on idk
@@ -21,14 +22,40 @@
             - {till}
         {/if}
     </p>
-    <a
-        slot="title"
-        href={gitHubLink}
-        class="leading-tight text-slate-200 font-semibold group/link text-lg"
-        target="_blank"
-        aria-label={`Personal project titled ${title} (opens in a new tab)`}
-        >{title}</a
-    >
+    <svelte:fragment slot="title">
+        <h3
+            class="leading-tight text-slate-200 font-semibold group/link text-lg"
+        >
+            {title}
+        </h3>
+        <div class="flex gap-2 mt-2">
+            <a
+                href={gitHubLink}
+                target="_blank"
+                aria-label={`${title}'s GitHub (opens in a new tab)`}
+                ><Icon
+                    name="GitHub"
+                    class="w-5 h-5 fill-slate-500 hover:fill-teal-200 transition duration-75 ease-out hover:scale-110"
+                /></a
+            >
+            {#if liveLink}
+                <a
+                    href={liveLink}
+                    target="_blank"
+                    class="text-slate-500 hover:text-teal-200 transition duration-75 ease-out group/link"
+                    aria-label={`A live link for ${title} (opens in a new tab)`}
+                >
+                    <Icon
+                        name="External"
+                        class="w-5 h-5 hover:scale-110 transition duration-75 ease-out"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={1.5}
+                    />
+                </a>
+            {/if}
+        </div>
+    </svelte:fragment>
     <svelte:fragment slot="description">
         <slot />
     </svelte:fragment>
--- src/components/projects/stinky.svelte
+++ src/components/projects/stinky.svelte
@@ -8,24 +8,40 @@
     <Title>Projects</Title>
     <Card
         title="Chop DB"
+        gitHubLink="https://github.com/Squshy/chop-db"
         from="2024"
         till="Present"
         technologies={['Rust', 'Go', 'gRPC']}
-        >A distributed, log structured database.</Card
+        >A distributed, log structured key-value store. Similar to Redis, this
+        database allows for quick retrieval and storage of persisted key-value
+        pairs. This project incorporates ideas presented in the book <a
+            href="https://dataintensive.net/"
+            target="_blank"
+            class="text-teal-100 hover:text-teal-200 transition duration-50 ease-out italic"
+            aria-label="Designing Data Intensive applications website (opens in new page)."
+            >Designing Data Intensive Applications</a
+        > by Martin Fowler.</Card
     >
     <Card
         title="Alertle"
+        gitHubLink="https://github.com/Squshy/alertle"
+        liveLink="https://www.npmjs.com/package/@alertle/react"
         from="2023"
         technologies={[
             'TypeScript',
             'React',
             'Vite',
+            'Vitest',
             'Changelogs',
             'TurboRepo',
-        ]}>A <i>blazingly fast</i>🔥 alert library for React.</Card
+        ]}
+        >A <i>blazingly fast</i>🔥 alert library for React applications. This
+        library incorporates a pub-sub model to update state in deeply nested
+        components without causing app wide re-renders.</Card
     >
     <Card
         title="TewDew"
+        gitHubLink="https://github.com/Squshy/TewDew"
         from="2023"
         technologies={[
             'Rust',
@@ -43,6 +59,8 @@
     >
     <Card
         title="Algorithm Showcase"
+        gitHubLink="https://github.com/Squshy/algorithm-showcase"
+        liveLink="https://algorithms.calvinlapp.ca/"
         from="2021"
         technologies={['TypeScript', 'NextJS', 'Dijkstra', 'Priority Queues']}
         >A display of different algorithms at work with an interactive Dijkstra
@@ -50,4 +68,13 @@
         interactive grid. This app also showcases how different sorting
         algorithms work by showing the sorts in real-time.</Card
     >
+    <p class="text-center text-sm">
+        These are a select few projects, more are listed on my <a
+            href="https://github.com/Squshy"
+            target="_blank"
+            aria-label="My personal GitHub (opens in a new page)."
+            class="text-teal-100 hover:text-teal-200 transition duration:50 ease-out"
+            >GitHub</a
+        >.
+    </p>
 </ContentWrapper>
